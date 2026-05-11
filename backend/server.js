e61ba89efae741b54095d3c5c2433142c26342ec
Jeritta <jerittajancy08@gmail.com>
2026-05-11 20:41:00 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,15 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
+app.options("*", cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
